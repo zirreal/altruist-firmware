1b92d3350593d56566b80774a330a69318de4745
Julia <uug11@mail.ru>
2025-12-16 18:35:31 +0400

    small refactor

--- display/driver/EPD_4in2_SSD1683.h
+++ display/driver/EPD_4in2_SSD1683.h
@@ -52,7 +52,10 @@ void EPD_4IN2_V2_Clear(void);
 void EPD_4IN2_V2_Display(UBYTE *Image);
 void EPD_4IN2_V2_Display_Fast(UBYTE *Image);
 void EPD_4IN2_V2_Display_4Gray(const UBYTE *Image);
+// Partial refresh (original API with region)
 void EPD_4IN2_V2_PartialDisplay(UBYTE *Image, UWORD Xstart, UWORD Ystart, UWORD Xend, UWORD Yend);
+// Convenience overload: partial refresh of the full screen
+void EPD_4IN2_V2_PartialDisplay(UBYTE *Image);
 void EPD_4IN2_V2_Sleep(void);
 
 #endif
